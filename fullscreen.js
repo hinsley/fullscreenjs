b7a4c3eb3b968eb025089cbd0847db60bbc5a386
Carter Hinsley <carterhinsley@gmail.com>
2015-08-13 14:55:45 -0400

    Use UMLJS/returnExports module design pattern
    Now FullscreenJS can be `require()`-ed in Node.js-style module systems.

--- fullscreen.js
+++ fullscreen.js
@@ -4,117 +4,133 @@
  * MIT License
  *****/
 
-/**
- * Attempt to activate fullscreen mode. If the provided element exposes the
- * fullscreen interface, return `true`, `false` otherwise.
- **/
-function activateFullscreen(element) {
-    if (element.requestFullscreen) {
-        element.requestFullscreen();
-        return true;
-    }
-    if (element.mozRequestFullScreen) {
-        element.mozRequestFullScreen();
-        return true;
-    }
-    if (element.msRequestFullscreen) {
-        element.msRequestFullscreen();
-        return true;
-    }
-    if (element.webkitRequestFullscreen) {
-        element.webkitRequestFullscreen();
-        return true;
-    }
-    return false;
-}
-
-/**
- * Deactivate fullscreen mode. Pretty simple.
- **/
-function deactivateFullscreen() {
-    if (document.exitFullscreen) {
-        document.exitFullscreen();
-        return true;
-    }
-    if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen();
-        return true;
-    }
-    if (document.msExitFullscreen) {
-        document.msExitFullscreen();
-        return true;
-    }
-    if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen();
-        return true;
-    }
-    return false;
-}
-
-/**
- * Register an event handler which is to be activated when fullscreen mode is
- * toggled on or off.
- **/
-function fullscreenChange(callback) {
-    var registerCallback = function (event_title) {
-        document.addEventListener(event_title, callback);
-    };
-    registerCallback('fullscreenchange');
-    registerCallback('mozfullscreenchange');
-    registerCallback('MSFullscreenChange');
-    registerCallback('webkitfullscreenchange');
-}
-
-/**
- * If fullscreen mode is active, return the element which is being displayed
- * in fullscreen view. Otherwise, return `null`.
- **/
-function fullscreenElement() {
-    return document.fullscreenElement    ||
-           document.mozFullScreenElement ||
-           document.msFullscreenElement  ||
-           document.webkitFullscreenElement;
-}
-
-/**
- * Return `true` if fullscreen mode is active, `false` otherwise.
- **/
-function fullscreenActive() {
-    return Boolean(fullscreenElement());
-}
-
-/**
- * Attempts to toggle fullscreen mode.
- **/
-function toggleFullscreen(element) {
-    if (fullscreenActive()) {
-        deactivateFullscreen();
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node.
+        module.exports = factory();
     } else {
-        activateFullscreen(element);
-    }
-}
+        // Browser globals (root is window).
+        root.returnExports = factory();
+  }
+}(this, function () {
+    return {
+        
+        /**
+         * Attempt to activate fullscreen mode. If the provided element exposes the
+         * fullscreen interface, return `true`, `false` otherwise.
+         **/
+        activateFullscreen: function (element) {
+            if (element.requestFullscreen) {
+                element.requestFullscreen();
+                return true;
+            }
+            if (element.mozRequestFullScreen) {
+                element.mozRequestFullScreen();
+                return true;
+            }
+            if (element.msRequestFullscreen) {
+                element.msRequestFullscreen();
+                return true;
+            }
+            if (element.webkitRequestFullscreen) {
+                element.webkitRequestFullscreen();
+                return true;
+            }
+            return false;
+        },
+        
+        /**
+         * Deactivate fullscreen mode. Pretty simple.
+         **/
+        deactivateFullscreen = function () {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+                return true;
+            }
+            if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+                return true;
+            }
+            if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+                return true;
+            }
+            if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+                return true;
+            }
+            return false;
+        },
+        
+        /**
+         * Register an event handler which is to be activated when fullscreen mode is
+         * toggled on or off.
+         **/
+        fullscreenChange = function (callback) {
+            var registerCallback = function (event_title) {
+                document.addEventListener(event_title, callback);
+            };
+            registerCallback('fullscreenchange');
+            registerCallback('mozfullscreenchange');
+            registerCallback('MSFullscreenChange');
+            registerCallback('webkitfullscreenchange');
+        },
 
-/**
- * Return `true` if fullscreen mode is supported by the current browser,
- * `false` otherwise.
- **/
-function fullscreenEnabled() {
-    return document.fullscreenEnabled    ||
-           document.mozFullScreenEnabled ||
-           document.msFullscreenEnabled  ||
-           document.webkitFullscreenEnabled;
-}
+        /**
+         * If fullscreen mode is active, return the element which is being displayed
+         * in fullscreen view. Otherwise, return `null`.
+         **/
+        fullscreenElement = function () {
+            return document.fullscreenElement    ||
+                   document.mozFullScreenElement ||
+                   document.msFullscreenElement  ||
+                   document.webkitFullscreenElement;
+        },
 
-/**
- * Register an event handler which is to be activated when fullscreen mode
- * triggers an error.
- **/
-function fullscreenError(callback) {
-    var registerCallback = function (event_title) {
-        document.addEventListener(event_title, callback);
+        /**
+         * Return `true` if fullscreen mode is active, `false` otherwise.
+         **/
+        fullscreenActive = function () {
+            return Boolean(fullscreenElement());
+        },
+
+        /**
+         * Attempts to toggle fullscreen mode.
+         **/
+        toggleFullscreen = function (element) {
+            if (fullscreenActive()) {
+                deactivateFullscreen();
+            } else {
+                activateFullscreen(element);
+            }
+        },
+
+        /**
+         * Return `true` if fullscreen mode is supported by the current browser,
+         * `false` otherwise.
+         **/
+        fullscreenEnabled = function () {
+            return document.fullscreenEnabled    ||
+                   document.mozFullScreenEnabled ||
+                   document.msFullscreenEnabled  ||
+                   document.webkitFullscreenEnabled;
+        },
+
+        /**
+         * Register an event handler which is to be activated when fullscreen mode
+         * triggers an error.
+         **/
+        fullscreenError = function (callback) {
+            var registerCallback = function (event_title) {
+                document.addEventListener(event_title, callback);
+            };
+            registerCallback('fullscreenerror');
+            registerCallback('mozfullscreenerror');
+            registerCallback('MSFullscreenError');
+            registerCallback('webkitfullscreenerror');
+        }
     };
-    registerCallback('fullscreenerror');
-    registerCallback('mozfullscreenerror');
-    registerCallback('MSFullscreenError');
-    registerCallback('webkitfullscreenerror');
-}
+}));
